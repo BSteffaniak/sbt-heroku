ec7150d7e04025d4ab6050d5055ed49b991ae7a0
Braden Steffaniak <BradenSteffaniak@gmail.com>
2020-04-24 12:00:45 -0500

    Add body-parser

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser');
+
 const PORT = process.env.PORT || 5000
 
-express()
+const app = express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(bodyParser.json())
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
